0b12fd76428a4520c3cfc6567cebf6e16b66e5fb
Adam Hałas <AgopnikH@gmail.com>
2023-03-27 19:29:46 +0200

    Introduce viewModel for canvas, fix drawing
    Put list of line on canvas to viewModel
    Fix drawing, draw lines over old ones instead under them

--- app/src/main/java/com/example/keepall/screens/CanvasScreen.kt
+++ app/src/main/java/com/example/keepall/screens/CanvasScreen.kt
@@ -1,10 +1,12 @@
 package com.example.keepall.screens
 
+import android.graphics.Bitmap
 import android.view.MotionEvent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -12,15 +14,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.createBitmap
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.keepall.model.Line
 import com.example.keepall.navigationbar.CanvasNavigationBar
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun CanvasScreen() {
+    val viewModel = viewModel<CanvasViewModel>()
+    val scope = rememberCoroutineScope()
+
+    var screenHeight = LocalConfiguration.current.screenHeightDp / LocalConfiguration.current.densityDpi
+    var screenWidth = LocalConfiguration.current.screenWidthDp / LocalConfiguration.current.densityDpi
+
+    val ctx = LocalContext.current
 
     var selectedColor by remember {
         mutableStateOf(Color.Black)
@@ -32,75 +50,102 @@ fun CanvasScreen() {
     var lastTouchY by remember {
         mutableStateOf(0f)
     }
+
+    val xd = viewModel.canvasStateFlow.collectAsState()
     val tempPath = remember {
         mutableStateOf<Line?>(Line())
     }
     var path = Line()
-    val lineList = remember {
-       mutableStateListOf<Line>()
-    }
     Scaffold(topBar = {
         CanvasNavigationBar(selectedColor) {
             selectedColor = it
+        }
+    },
+    floatingActionButton = {
+        FloatingActionButton(onClick = {
+            scope.launch {
+                val bitmap = viewModel.saveCanvasToJpg(screenWidth,screenHeight)
+                val dir = File("${ctx.filesDir.absolutePath}/CANVAS")
+                if(!dir.exists())
+                    dir.mkdir()
+                val file = File(dir.absolutePath, "testCanva")
+                file.createNewFile()
+                val fos = FileOutputStream(file)
+                fos.use {
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100,it)
+                }
+                fos.flush()
+                fos.close()
+            }.invokeOnCompletion {
+                //TODO Exit from canvas go to note
+                println("finished")
+            }
+        }) {
+
         }
     }) {
         Canvas(modifier = Modifier
             .fillMaxSize()
             .padding(it)
-            .pointerInteropFilter {
-                when (it.action) {
+            .pointerInteropFilter { event ->
+                when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        path.path.moveTo(it.x, it.y)
+                        path.path.moveTo(event.x, event.y)
                         path.color = selectedColor
-                        tempPath.value?.path?.moveTo(it.x, it.y)
+                        tempPath.value?.path?.moveTo(event.x, event.y)
                         tempPath.value?.color = selectedColor
-                        lastTouchX = it.x
-                        lastTouchY = it.y
+                        lastTouchX = event.x
+                        lastTouchY = event.y
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        val historySize = it.historySize
+                        val historySize = event.historySize
                         for (i in 0 until historySize) {
-                            val historyX = it.getHistoricalX(i)
-                            val historyY = it.getHistoricalY(i)
+                            val historyX = event.getHistoricalX(i)
+                            val historyY = event.getHistoricalY(i)
                             path.path.lineTo(historyX, historyY)
                             tempPath.value?.path?.lineTo(historyX, historyY)
                         }
-                        path.path.lineTo(it.x, it.y)
-                        tempPath.value?.path?.lineTo(it.x, it.y)
-                        lastTouchX = it.x
-                        lastTouchY = it.y
+                        path.path.lineTo(event.x, event.y)
+                        tempPath.value?.path?.lineTo(event.x, event.y)
+                        lastTouchX = event.x
+                        lastTouchY = event.y
                     }
                     MotionEvent.ACTION_UP -> {
-                        lineList.add(
+                        viewModel.addNewLine(
                             Line(
                                 path.path,
                                 path.color
                             )
                         )
                         path = Line()
+                        tempPath.value = Line()
                     }
                 }
 
-                lastTouchX = it.x
-                lastTouchY = it.y
+                lastTouchX = event.x
+                lastTouchY = event.y
                 val savePath = tempPath.value
                 tempPath.value = null
                 tempPath.value = savePath
                 true
+            }
+            .onSizeChanged { size ->
+                screenWidth = size.width
+                screenHeight = size.height
             }) {
-            tempPath.let {
+            xd.value.forEach { line ->
                 drawPath(
-                    path = it.value?.path ?: Path(),
-                    color = it.value?.color ?: Color.Black,
+                    path = line.path,
+                    color = line.color,
                     style = Stroke(
                         width = 4.dp.toPx()
                     )
                 )
             }
-            lineList.forEach {
+            tempPath.let { path ->
                 drawPath(
-                    path = it.path,
-                    color = it.color,
+                    path = path.value?.path ?: Path(),
+                    color = path.value?.color ?: Color.Black,
                     style = Stroke(
                         width = 4.dp.toPx()
                     )
---
+++ app/src/main/java/com/example/keepall/screens/CanvasViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.keepall.screens
+
+import android.graphics.Bitmap
+import android.graphics.Paint
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.core.graphics.applyCanvas
+import androidx.core.graphics.createBitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.keepall.model.Line
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class CanvasViewModel: ViewModel() {
+    private val _canvasStateFlow = MutableStateFlow<List<Line>>(listOf())
+    val canvasStateFlow = _canvasStateFlow
+
+
+
+    fun addNewLine(line: Line) {
+        val temp = _canvasStateFlow.value.toMutableList()
+        temp.add(line)
+        _canvasStateFlow.value = temp.toList()
+    }
+
+    fun saveCanvasToJpg(width: Int, height: Int): Bitmap {
+        val bitmap = createBitmap(width, height)
+        return bitmap.applyCanvas {
+            this.drawARGB(255,255,255,255)
+            canvasStateFlow.value.forEach {
+                val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+                paint.style = Paint.Style.STROKE
+                paint.color = it.color.hashCode()
+                paint.strokeWidth = 4f
+                this.drawPath(
+                    it.path.asAndroidPath(),
+                    paint
+                )
+            }
+        }
+    }
+}
