50bcdc04d03ea03fd2db2a81e910ea902fad0dca
Adam Halas <adam.halas1132@gmail.com>
2023-04-27 19:18:13 +0200

    Updated NoteSnippet

--- app/src/main/java/com/example/keepall/components/NoteSnippet.kt
+++ app/src/main/java/com/example/keepall/components/NoteSnippet.kt
@@ -8,42 +8,52 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.keepall.R
 import com.example.keepall.data.Note
 import com.example.keepall.ui.theme.Yellow20
 import com.example.keepall.utils.fromStringToList
+import com.example.keepall.utils.wrap
 
 @Composable
-fun NoteSnippet(note: Note, onClick:(Int) -> Unit) {
-    val canvasCount = if(!note.canvas.isNullOrBlank()) 1 else 0
+fun NoteSnippet(
+    modifier: Modifier,
+    note: Note
+) {
+    val canvasCount = if (!note.canvas.isNullOrBlank()) 1 else 0
     val photosCount = fromStringToList(note.devicePhotos).size.also {
         if (!note.photo.isNullOrBlank())
             it + 1
     }
-    Box(
-        modifier = Modifier
-            .clip(RoundedCornerShape(10))
-            .background(color = Yellow20)
-            .clickable { onClick(note.id) }
-            .padding(
-                PaddingValues(
-                    top = 10.dp, start = 10.dp, end = 10.dp, bottom = 0.dp
-                )
-            )
-    ) {
-        Column {
-            Text(text = note.textContent)
+//        .background(color = Yellow20)
+//        .heightIn(100.dp, 200.dp)
+
+
+    Scaffold(modifier = modifier,
+        containerColor = Yellow20,
+        contentWindowInsets = WindowInsets(10.dp,10.dp,10.dp,0.dp),
+        bottomBar = {
             NoteBottomBar(canvasCount, photosCount, false)
-        }
+        }) {
+        Text(
+            modifier = Modifier
+                .padding(it),
+            softWrap = true,
+            text = note.textContent,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
+
 
 @Composable
 fun NoteBottomBar(canvasCount: Int, photosCount: Int, isInCalendar: Boolean) {
@@ -52,21 +62,29 @@ fun NoteBottomBar(canvasCount: Int, photosCount: Int, isInCalendar: Boolean) {
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Image(painter = painterResource(id = R.drawable.palette_24px), contentDescription = "canvas count")
+            Image(
+                painter = painterResource(id = R.drawable.palette_24px),
+                contentDescription = "canvas count"
+            )
             Text(text = canvasCount.toString())
         }
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Image(painter = painterResource(id = R.drawable.image_24px), contentDescription = "photos count")
+            Image(
+                painter = painterResource(id = R.drawable.image_24px),
+                contentDescription = "photos count"
+            )
             Text(text = photosCount.toString())
         }
 
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Image(painter =
-            if (isInCalendar)
-            painterResource(id = R.drawable.event_available_24px)
+            Image(
+                painter =
+                if (isInCalendar)
+                    painterResource(id = R.drawable.event_available_24px)
                 else
-                painterResource(id = R.drawable.event_busy_24px),
-                contentDescription = "canvas count")
+                    painterResource(id = R.drawable.event_busy_24px),
+                contentDescription = "canvas count"
+            )
         }
 
 
--- app/src/main/java/com/example/keepall/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/keepall/screens/homescreen/HomeScreen.kt
@@ -1,21 +1,27 @@
 package com.example.keepall.screens.homescreen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.keepall.components.AddNoteFAB
 import com.example.keepall.components.NoteSnippet
 import com.example.keepall.navigationbar.KeepAllNavigationBar
 import com.example.keepall.screens.homescreen.HomeViewModel
+import com.example.keepall.ui.theme.Yellow20
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: HomeViewModel) {
@@ -27,14 +33,20 @@ fun HomeScreen(navController: NavController, viewModel: HomeViewModel) {
         floatingActionButton = {
             AddNoteFAB(navController = navController)
         }) {
-        LazyColumn(modifier = Modifier.padding(it),
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(10.dp),
+            modifier = Modifier.padding(it),
             verticalArrangement = Arrangement.spacedBy(10.dp),
-        contentPadding = PaddingValues(10.dp)
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             items(notesList.value) {note ->
-                NoteSnippet(note) {
-
-                }
+                NoteSnippet(modifier = Modifier
+                    .clip(RoundedCornerShape(5))
+                    .height(200.dp)
+                    .clickable {  },
+                    note = note)
             }
 
         }
