485e80cfca0f460dcf8997da3f44f6fd85b21ab6
Adam Halas <adam.halas1132@gmail.com>
2023-05-01 14:35:08 +0200

    Small changes to CameraScreen and CameraBottomBar
    Made CameraBottomBar composable take content instead of things that might lead to leak

--- app/src/main/java/com/example/keepall/components/CameraBottomBar.kt
+++ app/src/main/java/com/example/keepall/components/CameraBottomBar.kt
@@ -7,14 +7,12 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -29,90 +27,12 @@ import java.util.concurrent.Executor
 
 @Composable
 fun CameraBottomBar(
-    imageCapture: ImageCapture,
-    executor: Executor,
-    context: Context,
-    viewModel: CameraViewModel,
-    state: CameraUiState
+    content: @Composable () -> Unit
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        when (state) {
-            is CameraUiState.Success -> {
-                Image(
-                    modifier = Modifier
-                        .size(72.dp)
-                        .clickable {
-                            viewModel.goBackToPreview()
-                        },
-                    imageVector = Icons.Default.Close,
-                    contentDescription = "Reject photo"
-                )
-                Image(
-                    modifier = Modifier
-                        .size(72.dp)
-                        .clickable {
-                            context as Activity
-                            val intent = Intent().also {
-                                it.putExtra(PHOTO_PATH, state.path)
-                            }
-                            context.setResult(Activity.RESULT_OK, intent)
-                            context.finish()
-                        },
-                    imageVector = Icons.Default.Check,
-                    contentDescription = "Accept photo"
-                )
-            }
-            else -> {
-                Image(
-                    modifier = Modifier
-                        .size(72.dp)
-                        .clickable {
-                            saveImage(imageCapture, executor, context, viewModel)
-                        },
-                    painter = painterResource(id = R.drawable.take_photo),
-                    contentDescription = "Take photo"
-                )
-            }
-        }
-
+        content()
     }
-}
-
-private fun saveImage(
-    imageCapture: ImageCapture,
-    executor: Executor,
-    context: Context,
-    viewModel: CameraViewModel
-) {
-
-    val imageFolder = File("${context.filesDir.absolutePath}/PHOTOS")
-    if (!imageFolder.exists())
-        imageFolder.mkdir()
-
-    val outputFile = File(
-        imageFolder,
-        "${
-            SimpleDateFormat(
-                "yyyyMMddhhmmss",
-                Locale.getDefault()
-            ).format(System.currentTimeMillis())
-        }.jpg"
-    )
-    outputFile.createNewFile()
-    val outputOptions = ImageCapture.OutputFileOptions.Builder(outputFile).build()
-    imageCapture.takePicture(outputOptions, executor, object : ImageCapture.OnImageSavedCallback {
-        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-            viewModel.sendPhoto(outputFile.canonicalPath)
-        }
-
-        override fun onError(exception: ImageCaptureException) {
-            outputFile.delete()
-            viewModel.sendError("Error while taking a photo")
-        }
-
-    })
-
 }
--- app/src/main/java/com/example/keepall/screens/camera/CameraScreen.kt
+++ app/src/main/java/com/example/keepall/screens/camera/CameraScreen.kt
@@ -1,31 +1,46 @@
 package com.example.keepall.screens.camera
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.camera.core.CameraEffect
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.keepall.R
 import com.example.keepall.components.CameraBottomBar
+import com.example.keepall.constants.PHOTO_PATH
 import com.example.keepall.uievents.CameraUiState
 import kotlinx.coroutines.launch
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executor
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 import kotlin.math.roundToInt
@@ -40,15 +55,54 @@ fun CameraScreen() {
         .build()
     val viewModel = viewModel<CameraViewModel>()
     val cameraUiState = viewModel.cameraUiState.collectAsState()
+    val type = remember {
+        mutableStateOf(0)
+    }
+    //0 back
+    //1 front
     Scaffold(
         bottomBar = {
-            CameraBottomBar(
-                imageCapture,
-                mainExecutor,
-                localContext,
-                viewModel,
-                cameraUiState.value
-            )
+            CameraBottomBar() {
+                when (cameraUiState.value) {
+                    is CameraUiState.Success -> {
+                        Image(
+                            modifier = Modifier
+                                .size(72.dp)
+                                .clickable {
+                                    viewModel.goBackToPreview()
+                                },
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Reject photo"
+                        )
+                        Image(
+                            modifier = Modifier
+                                .size(72.dp)
+                                .clickable {
+                                    localContext as Activity
+                                    val intent = Intent().also {
+                                        it.putExtra(PHOTO_PATH, (cameraUiState.value as CameraUiState.Success).path)
+                                    }
+                                    localContext.setResult(Activity.RESULT_OK, intent)
+                                    localContext.finish()
+                                },
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "Accept photo"
+                        )
+                    }
+                    else -> {
+                        Image(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .padding(bottom = 0.dp)
+                                .clickable {
+                                    saveImage(imageCapture, mainExecutor, localContext, viewModel)
+                                },
+                            painter = painterResource(id = R.drawable.take_photo),
+                            contentDescription = "Take photo"
+                        )
+                    }
+                }
+            }
         },
     ) { paddingValues ->
         AndroidView(factory = { context ->
@@ -119,4 +173,40 @@ fun PreviewTakenPhoto(padding: PaddingValues, bitmap: Bitmap) {
             contentDescription = "Preview of the photo"
         )
     }
+}
+
+private fun saveImage(
+    imageCapture: ImageCapture,
+    executor: Executor,
+    context: Context,
+    viewModel: CameraViewModel
+) {
+
+    val imageFolder = File("${context.filesDir.absolutePath}/PHOTOS")
+    if (!imageFolder.exists())
+        imageFolder.mkdir()
+
+    val outputFile = File(
+        imageFolder,
+        "${
+            SimpleDateFormat(
+                "yyyyMMddhhmmss",
+                Locale.getDefault()
+            ).format(System.currentTimeMillis())
+        }.jpg"
+    )
+    outputFile.createNewFile()
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(outputFile).build()
+    imageCapture.takePicture(outputOptions, executor, object : ImageCapture.OnImageSavedCallback {
+        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+            viewModel.sendPhoto(outputFile.canonicalPath)
+        }
+
+        override fun onError(exception: ImageCaptureException) {
+            outputFile.delete()
+            viewModel.sendError("Error while taking a photo")
+        }
+
+    })
+
 }
